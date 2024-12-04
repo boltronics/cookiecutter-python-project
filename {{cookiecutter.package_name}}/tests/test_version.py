316cfba254bce8c29311ac0bda88509836b6547c
Adam Bolte <abolte@systemsaviour.com>
2024-12-04 19:30:18 +1100

    Minor pylint fixes to examples and tests

--- {{cookiecutter.package_name}}/tests/test_version.py
+++ {{cookiecutter.package_name}}/tests/test_version.py
@@ -1,3 +1,5 @@
+"""Version unit tests"""
+
 import unittest
 
 import {{cookiecutter.package_name}}
@@ -6,7 +8,7 @@ import {{cookiecutter.package_name}}
 class VersionTestCase(unittest.TestCase):
     """Version tests"""
 
-    def test_version(self):
+    def test_version(self) -> None:
         """check {{cookiecutter.package_name}} exposes a version attribute"""
         self.assertTrue(hasattr({{cookiecutter.package_name}}, "__version__"))
         self.assertIsInstance({{cookiecutter.package_name}}.__version__, str)
